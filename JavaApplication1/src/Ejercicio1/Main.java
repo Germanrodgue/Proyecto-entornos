9ca3678c49dad4fbbd213b94fa91e289de265227
Germanrodgue <Germanrodgue@gmail.com>
2017-02-04 01:39:04 +0100

    Proyecto entornos

---
+++ JavaApplication1/src/Ejercicio1/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejercicio1;
+
+import Ejercicio6.modelos_cuadros_de_lista2;
+
+/**
+ *
+ * @author sonfil
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+        
+    public static void main(String[] args) {
+                java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Formulario().setVisible(true);
+            }
+        });
+    }
+    
+}
